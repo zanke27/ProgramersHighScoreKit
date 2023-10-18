da502543614f4b78b8d8e52ae6fcf08b5c979735
zanke27 <zanke27@gmail.com>
2023-10-19 00:44:33 +0900

    누락된 문제 추가

--- 올바른 괄호.cpp
+++ 올바른 괄호.cpp
@@ -4,8 +4,22 @@ using namespace std;
 
 int main(void)
 {
-	string s = "(())()";
-	string s1 = ")(()";
+    string s = "()()";
 
+    bool answer = true;
 
+    int result = 0;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (result < 0) break;
+
+        if (s[i] == '(') result++;
+        else if (s[i] == ')') result--;
+    }
+
+    if (result == 0) answer = true;
+    else answer = false;
+
+    cout << answer;
 }
